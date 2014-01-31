ea23ebe985193b146ec78d0d2c8c3f1f32d439f6
klisha <mans1@ngs.ru>
2014-01-31 21:59:01 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hockey::Application.config.secret_token = 'c4460f34a2077a83755cef695ae6736a740f60f6ee8747b136f13e391536a2ad2f89b0f19f3b1e47e901d9ec689f68714dbe4db87c81627d977deb4f5e4f8ba0'
