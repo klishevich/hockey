623b89db205702e892f73de0422f60530d4cf12f
klisha <mans1@ngs.ru>
2014-05-24 14:50:32 +0400

    can create events html

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,8 @@ class Client < ActiveRecord::Base
   has_many :events
   attr_accessible :address, :city, :country, :fullname, :index, 
                   :inn, :kpp, :ogrn, :shortname, :client_type
+
+  validates :shortname, presence: true
+  validates :fullname, presence: true
+  validates :inn, presence: true  
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :client
   attr_accessible :event_description, :event_type
+  validates :event_description, presence: true
 end
