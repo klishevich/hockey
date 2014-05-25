7e3d107d47711344c67418bb03b19b5f247750e2
klisha <mans1@ngs.ru>
2014-05-25 23:13:53 +0400

    token authentication

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,7 +1,9 @@
 module Api
   module V1
     class ClientsController < ApplicationController
-
+      # http_basic_authenticate_with name: "admin", password: "secret"
+      # curl  http://localhost:3000/api/clients?i408290560 -u 'admin:secret'
+      before_filter :restrict_access
       respond_to :json
 
       def index
@@ -9,57 +11,67 @@ module Api
         kpp = params[:kpp]
         @clients = kpp.blank? ? Client.where(["inn = ?", inn]) 
                               : Client.where(["inn = ? and kpp = ?", inn, kpp])
-        # @clients = Client.all
-        # respond_to do |format|
-        #   format.json {@clients = kpp.blank? ? Client.where(["inn = ?", inn]) 
-        #                     : Client.where(["inn = ? and kpp = ?", inn, kpp])}
-        #   format.html {@clients ||= Client.all}
-        # end
-        respond_with @clients
+        # respond_with @clients
+        render :json => @clients, :include => :events
       end
 
       def show
         @client = Client.find(params[:id])
         @events = @client.events
+        render :json => @client, :include => :events
         # respond_to do |format|  
         #   format.html
         #   format.json  { render :json => @client, :include => :events}
         # end
       end
 
-      def new
-        @client = Client.new
-      end
+      # def new
+      #   @client = Client.new
+      # end
 
-      def edit
-        @client = Client.find(params[:id])
-      end
+      # def edit
+      #   @client = Client.find(params[:id])
+      # end
 
       def create
-        @client = Client.new(params[:client])
+       #  @client = Client.new(params[:client])
 
-    	  if @client.save
-    	    redirect_to @client, notice: 'client was successfully created.'
-    	  else
-    	    render action: "new"
-    	  end
+    	  # if @client.save
+    	  #   redirect_to @client, notice: 'client was successfully created.'
+    	  # else
+    	  #   render action: "new"
+    	  # end
+        respond_with Client.create(params[:client])
       end
 
       def update
-        @client = Client.find(params[:id])
+        # @client = Client.find(params[:id])
 
-        if @client.update_attributes(params[:client])
-          redirect_to @client, notice: 'client was successfully updated.'
-        else
-          render action: "edit"
-        end
+        # if @client.update_attributes(params[:client])
+        #   redirect_to @client, notice: 'client was successfully updated.'
+        # else
+        #   render action: "edit"
+        # end
+        respond_with Client.update(params[:id], params[:client])
       end
 
       def destroy
-        @client = Client.find(params[:id])
-        @client.destroy
+        # @client = Client.find(params[:id])
+        # @client.destroy
 
-        redirect_to clients_url
+        # redirect_to clients_url
+        respond_with Client.destroy(params[:id])
+      end
+
+      private
+      # def restrict_access
+      #   api_key = ApiKey.find_by_access_token(params[:access_token])
+      #   head :unauthorized unless api_key
+      # end
+      def restrict_access
+        authenticate_or_request_with_http_token do |token, options|
+          ApiKey.exists?(access_token: token)
+        end
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140518193534) do
+ActiveRecord::Schema.define(:version => 20140525182143) do
+
+  create_table "api_keys", :force => true do |t|
+    t.string   "access_token"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "articles", :force => true do |t|
     t.string   "title"
